31a5eba5fbbcdb31ef92fb27640db68068d6c293
bookkuun <bookkuun@gmail.com>
2024-11-04 10:57:37 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const api = require("./routes/api");
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use("/api", api);
+
+app.listen(port, () => {
+  console.log("Server is running");
+});
